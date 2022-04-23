1c216d4211ce4fe9bf62a1f5e16de85cc469fe83
theyangpaopao <38746708+theyangpaopao@users.noreply.github.com>
2022-04-23 20:04:44 +0800

    Initial commit

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/yanghao/Documents/class1
+# Install script for directory: /Users/yanghao/Documents/git_project/class1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/yanghao/Documents/class1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/yanghao/Documents/git_project/class1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
